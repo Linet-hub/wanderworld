718c2bcb72f561fb8b57e267abba63962ca224bc
Linet-hub <gathonilinet236@gmail.com>
2024-08-20 13:05:54 +0300

    first commit

---
+++ app/src/main/java/com/example/wanderworld/navigation/AppNavHost.kt
@@ -0,0 +1,59 @@
+package com.example.wanderworld.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+import com.example.wanderworld.ui.theme.screens.about.AboutScreen
+
+import com.example.wanderworld.ui.theme.screens.home.HomeScreen
+import com.example.wanderworld.ui.theme.screens.login.LoginScreen
+
+import com.example.wanderworld.ui.theme.screens.signup.SignupScreen
+import com.example.wanderworld.ui.theme.screens.splash.SplashScreen
+
+@Composable
+fun AppNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination:String = ROUT_SPLASH
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+
+        composable(ROUT_HOME) {
+            HomeScreen(navController = navController)
+        }
+
+
+        composable(ROUT_ABOUT) {
+            AboutScreen(navController = navController)
+        }
+
+        composable(ROUT_SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
+
+        composable(ROUT_SIGNUP) {
+            SignupScreen(navController = navController)
+        }
+
+        composable(ROUT_LOGIN) {
+            LoginScreen(navController = navController)
+        }
+
+
+
+
+
+
+
+    }
+}
